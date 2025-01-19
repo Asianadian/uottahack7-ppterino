4cf2967b116a2c4b7db47c40eb4e4980b94cc0f6
Sydney Nguyen <sydney2004nguyen@gmail.com>
2025-01-19 04:26:38 -0500

    Make Pizza Rotate

--- app/components/Experience.jsx
+++ app/components/Experience.jsx
@@ -1,13 +1,25 @@
+import React, { useRef } from "react";
 import { OrbitControls } from "@react-three/drei";
-import {Pizza} from "./Pizza";
+import { useFrame } from "@react-three/fiber";
+import { Pizza } from "./Pizza";
 
 export const Experience = () => {
-  
+  const pizzaRef = useRef();
+
+  // Rotate the pizza in each frame
+  useFrame(() => {
+    if (pizzaRef.current) {
+      pizzaRef.current.rotation.y += 0.01; // Adjust rotation speed as needed
+    }
+  });
+
   return (
     <>
       <OrbitControls />
-      <ambientLight intensity={5} />
-      <Pizza />
+      <ambientLight intensity={6} />
+      <group ref={pizzaRef}>
+        <Pizza />
+      </group>
     </>
   );
 };
